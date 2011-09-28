c86b6c7dba4feeb3d2632e19950cb8fa26b55f79
Alberto Penas Amor <alberto__pa@hotmail.com>
2011-09-28 20:48:06 +0200

    spirtes over sprite fixed

--- src/es/dev/game/config/Constants.java
+++ src/es/dev/game/config/Constants.java
@@ -11,6 +11,11 @@ public interface Constants {
 	public static int PLAY_METHOD_BED = 1;
 	public static int PLAY_METHOD_HORIZONTAL = 2;
 	
+	public static int PAR = 0;
+	public static int DOS = 2;
+	public static int TRES = 3;
+	public static int ZERO = 0;
+	
 	
 	
 	public static Boolean DEBUG = true;
